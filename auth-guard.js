8a7ce5f64d841c64a2d1b9706ce26f034d8988c1
mudamudisedahromolor <mudamudisedahromolor@gmail.com>
2026-06-01 15:36:50 +0100

    Update auth-guard.js

--- auth-guard.js
+++ auth-guard.js
@@ -1,5 +1,5 @@
 // auth-guard.js
-if (sessionStorage.getItem("statusLogin") !== "aktif") {
+if (sessionStorage.getItem("statusAdmin") !== "aktif") {
     alert("Akses ditolak! Silakan login terlebih dahulu melalui portal.");
     window.location.href = "index.html"; // Arahkan ke nama file portal loginmu
 }
