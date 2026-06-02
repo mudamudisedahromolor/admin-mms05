a5957bb0dd47b1331321fc8abcfb420ad744497d
mudamudisedahromolor <mudamudisedahromolor@gmail.com>
2026-06-02 10:21:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -871,7 +871,7 @@ window.muatBaganLombaVisual = function() {
             });
             container.appendChild(elemenRonde);
         }
-    })
+    };
     .catch(err => {
         container.innerHTML = `<p style="text-align: center; color: #e53935; width: 100%; font-weight: bold;"><i class="fa-solid fa-triangle-exclamation"></i> Gagal terhubung ke server robot. Pastikan deployment Apps Script benar.</p>`;
     });
